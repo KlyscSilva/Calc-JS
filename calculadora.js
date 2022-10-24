45a5f8aeba603ec115aec3e6e1d331f81c40552d
Matheus Silva <114446363+KlyscSilva@users.noreply.github.com>
2022-10-24 16:43:58 -0300

    up

--- calculadora.js
+++ calculadora.js
@@ -1,15 +1,38 @@
 function inserirDadosDisplay(component) {
     let tela = document.querySelector(".display");
+    let telaResultado = document.querySelector(".display2");
     if (component.value !== "AC") {
         tela.value = tela.value + component.value;
+        resultadoExpressao(component);
     }
     if (component.value === "AC") {
+        telaResultado.value = "";
         tela.value = "";
     }
 }
 
 function resultadoExpressao(component) {
     let tela = document.querySelector(".display");
+    let telaResultado = document.querySelector(".display2");
+    if (tela) { //verifica se a tela esta disponivel
+        if (tela.value === "" && component.value === "=") { //impede que o usuario aperte o botao igual sem ter expressao na tela
+            tela.value = "";
+            telaResultado = "";
+        }
+        else {
+            if (eval(tela.value) == Number.POSITIVE_INFINITY || eval(tela.value) == Number.NEGATIVE_INFINITY || eval(tela.value) == NaN) {
+                telaResultado.value = "Ind."; //tratamento de indeterminacoes matematicas
+            }
+            else {
+                telaResultado.value = eval(tela.value); //expressao matematica valida
+            }
+        }
+    }
+}
+
+function resultadoIgual(component) {
+    let tela = document.querySelector(".display");
+    let telaResultado = document.querySelector(".display2");
     if (tela) { //verifica se a tela esta disponivel
         if (tela.value === "" && component.value === "=") { //impede que o usuario aperte o botao igual sem ter expressao na tela
             tela.value = "";
@@ -19,9 +42,10 @@ function resultadoExpressao(component) {
                 tela.value = "Ind."; //tratamento de indeterminacoes matematicas
             }
             else {
-                tela.value = eval(tela.value); //expressao matematica valida
+                var resultado = eval(tela.value); //expressao matematica valida
+                tela.value = resultado;
+                telaResultado.value = ""; //apaga  o que esta no display2
             }
         }
     }
 }
-
